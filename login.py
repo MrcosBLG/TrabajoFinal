b7fe6536cc50fcefeeafbab256ae24aec1fd264a
ulisesstreluk <ulises.nob123@gmail.com>
2024-10-15 11:25:04 -0300

    login

--- login.py
+++ login.py
@@ -1,19 +1,24 @@
-usuario = input('Escribe tu usuario =>')
-usuario_valido = 'ucel123'
-
-while usuario!= usuario_valido: 
- usuario= input("usuario incorrecto, intente con un usuiario valido: ")
+def login():
     
-if usuario  in usuario_valido:
- print('usuario valido, puedes continuar')
+    usuario = input('Escribe tu usuario =>')
+    usuario_valido = 'ucel123'
+    
+    while usuario!= usuario_valido: 
+        usuario= input("usuario incorrecto, intente con un usuiario valido: ")
+        
+    if usuario  in usuario_valido:
+        print('usuario valido, puedes continuar')
  
-contraseña = input('ingresa tu contrasena =>')
-contraseña_valida = "123"
-
-while contraseña != contraseña_valida:
-    contraseña = input("la contraseña es incorrecta, intente con una contraseña valida: ")
+    contraseña = input('ingresa tu contrasena =>')
+    contraseña_valida = "123"
    
-if contraseña == contraseña_valida:
-    print('usuario valido')
-else:
-  print("usuario invalido, intente denuevo")
+    while contraseña != contraseña_valida:
+        contraseña = input("la contraseña es incorrecta, intente con una contraseña valida: ")
+   
+    if contraseña == contraseña_valida:
+        print('usuario valido')
+    else:
+        print("usuario invalido, intente denuevo")
+if __name__ == "__main__":
+    login()
+
