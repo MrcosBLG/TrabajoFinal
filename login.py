fe257d6e177096f522e576ed80660fdb54d269ab
ulisesstreluk <ulises.nob123@gmail.com>
2024-10-15 10:52:26 -0300

    este es el login

---
+++ login.py
@@ -0,0 +1,19 @@
+usuario = input('Escribe tu usuario =>')
+usuario_valido = 'ucel123'
+
+while usuario!= usuario_valido: 
+ usuario= input("usuario incorrecto, intente con un usuiario valido: ")
+    
+if usuario  in usuario_valido:
+ print('usuario valido, puedes continuar')
+ 
+contraseña = input('ingresa tu contrasena =>')
+contraseña_valida = "123"
+
+while contraseña != contraseña_valida:
+    contraseña = input("la contraseña es incorrecta, intente con una contraseña valida: ")
+   
+if contraseña == contraseña_valida:
+    print('usuario valido')
+else:
+  print("usuario invalido, intente denuevo")
