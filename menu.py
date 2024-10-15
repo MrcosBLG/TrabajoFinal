f11833aa9836256f4e6fdeec305f025257c56a2a
ulisesstreluk <ulises.nob123@gmail.com>
2024-10-15 19:50:01 -0300

    menu

--- menu.py
+++ menu.py
@@ -1,2 +1,15 @@
-stock = 000
-aa = 78
+
+from login import login
+
+if __name__ == "__main__":
+    if login():
+        print("menu")
+    else:
+        login()
+
+productos=[]
+productos_agotados=[]
+productos_en_stock=[]
+ventas=[]
+ingresa_productos=[]
+extraer_priductos=[]
